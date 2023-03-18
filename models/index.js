8966cd890418b3eae40750ffa5c7dec9d85b2bff
dmantilla17 <derekaraujo11@gmail.com>
2023-03-17 22:59:45 -0400

    added to the index.js file in models

--- models/index.js
+++ models/index.js
@@ -1,11 +1,13 @@
 // import models
-const Product = require('./Product');
-const Category = require('./Category');
-const Tag = require('./Tag');
-const ProductTag = require('./ProductTag');
+const Product = require("./Product");
+const Category = require("./Category");
+const Tag = require("./Tag");
+const ProductTag = require("./ProductTag");
 
 // Products belongsTo Category
-Product.belongsTo(Category, {});
+Product.belongsTo(Category, {
+  foreignKey: "pr",
+});
 
 // Categories have many Products
 
